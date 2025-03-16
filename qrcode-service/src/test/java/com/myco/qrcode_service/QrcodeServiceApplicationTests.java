dbb62e81422b02a1da033ccf8e50bda50ccf3d34
Shekhar Kumar <shekharkumargupta@gmail.com>
2025-03-16 16:32:31 +0530

    created spring boot fresh project

---
+++ qrcode-service/src/test/java/com/myco/qrcode_service/QrcodeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myco.qrcode_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

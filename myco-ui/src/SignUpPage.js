103ce61af1ba539cc358ff761feecf44d1748cb0
Shekhar Kumar <shekharkumargupta@gmail.com>
2025-03-28 22:24:02 +0530

    added bootstrap

--- myco-ui/src/SignUpPage.js
+++ myco-ui/src/SignUpPage.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const SignUpPage = () => {
   const [mobileNumber, setMobileNumber] = useState('');

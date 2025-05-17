af3fb50249860669e92f72a04a1aeec7c1116565
Shekhar Kumar <shekharkumargupta@gmail.com>
2025-05-17 23:40:23 +0530

    renamed main class name as MyCoApplication

--- src/main/java/com/myco/users/MyCoApplication.java
+++ src/main/java/com/myco/users/MyCoApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserServiceApplication {
+public class MyCoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
+		SpringApplication.run(MyCoApplication.class, args);
 	}
 }
